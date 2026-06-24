5db4dccb5f85abbac75a2921923c4f460db45adf
Keshav Thathai <thataikeshav873@gmail.com>
2026-06-24 15:42:19 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,22 +1,7 @@
 import streamlit as st
 
 
-st.title("
-
-|  _________________  |
-| |              0. | |
-| |_________________| |
-|  ___ ___ ___  ___   |
-| | 7 | 8 | 9 | | / | |
-| |___|___|___| |___| |
-| | 4 | 5 | 6 | | * | |
-| |___|___|___| |___| |
-| | 1 | 2 | 3 | | - | |
-| |___|___|___| |___| |
-| | . | 0 | = | | + | |
-| |___|___|___| |___| |
-|_____________________|
-Pro Smart Calculator")
+st.title("Pro Smart Calculator")
 st.write("Welcome to your simple and fast installable calculator app.")
 st.divider()
 
